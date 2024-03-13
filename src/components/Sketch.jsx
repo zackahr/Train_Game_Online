b878bf4e0f8a5fac008a36abaf34b6e5a9b21ee9
zezzine <ziko.isezzine@gmail.com>
2024-03-13 23:53:39 +0000

    add gitignore

--- src/components/Sketch.jsx
+++ src/components/Sketch.jsx
@@ -16,13 +16,14 @@ const Sketch = () => {
   console.log("edge ", edgeTrain)
   // const [falling, setFalling] = useState(false);
   var falling = false;
-
+  var win = false;
+  
   useEffect(() => {
     const sketch = new p5((p) => {
       p.setup = () => {
         canvas = p.createCanvas(sketchW, sketchH).style('border', '1px solid #fff');
       };
-
+      
       p.draw = () => {
         p.background(0);
         p.rect(x, y2 - pointSize / 2, pointSize, pointSize);
@@ -42,13 +43,13 @@ const Sketch = () => {
         p.line(0, y + 10, edgeTrain, y + 10);
         
         p.drawingContext.setLineDash([20, 0]); // set line dash pattern
-        p.line(x + pointSize / 2, y - 10, sketchW, y - pointSize * 2);
-        p.line(x + pointSize / 2, y - 10, sketchW, y - pointSize * 2.5);
-        p.line(x + pointSize / 2, y - 10, sketchW, y - pointSize * 3);
-        p.line(x + pointSize / 2, y - 10, sketchW, y - pointSize * 3.5);
-        p.line(x + pointSize / 2, y - 10, sketchW, y - pointSize * 4);
+        let trainLight = x + 400;
+        p.line(x + pointSize , y, trainLight, y - pointSize * 2);
+        p.line(x + pointSize , y, trainLight, y - pointSize * 3);
+        p.line(x + pointSize , y, trainLight, y - pointSize * 4);
+        p.line(x + pointSize , y, trainLight, y - pointSize * 5);
+        p.line(x + pointSize , y, trainLight, y - pointSize * 6);
 
-        // x =980 fall 
         if (x >= 980 && edgeTrain === 1000) {
           falling = true;
         }
@@ -56,11 +57,14 @@ const Sketch = () => {
         if (falling) {
           y2 += moveAmount * 0.3;
           p.background(0);
-
-          // text color
           p.fill(111);
           p.textSize(50);
           p.text('Game Over', sketchW / 2 - 100, sketchH / 2);
+        } if (win) {
+          p.background(0);
+          p.fill(111);
+          p.textSize(50);
+          p.text('Both OF The Players Win', sketchW / 2.5 - 100, sketchH / 2);
         }
 
         if (y > sketchH) {
@@ -82,9 +86,14 @@ const Sketch = () => {
       if (event.key === 'ArrowLeft') {
         x = Math.max(x - moveAmount, pointSize / 2);
       } else if (event.key === 'ArrowRight') {
-        x = Math.min(x + moveAmount, sketchW - pointSize / 2);
-      } else if (event.key === 'ArrowUp') {
+        x = Math.min(x + moveAmount, sketchW - pointSize);
+        if (x == sketchW - pointSize) {
+          win = true;
+        }
+      } else if (event.key === 'ArrowUp' && edgeTrain < 1200) {
         edgeTrain += 200;
+      } else if (event.key === 'ArrowDown' && edgeTrain > 1000) {
+        edgeTrain -= 200;
       }
     };
 
